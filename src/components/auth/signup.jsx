7868bb34bc081e7b3113a9bd02ca6936d6c5c44b
kajal kohli <kohlikajal00@gmail.com>
2025-06-02 18:36:32 +0530

    finish signup page

--- src/components/auth/signup.jsx
+++ src/components/auth/signup.jsx
@@ -1,8 +1,44 @@
 import "./signup.css";
 export default function Signup() {
     return(
-       <div>
+        <div class="signupcontainer">
+        <form class="signup-box">
+      <h2 class="logo">LearnHub</h2>
+      <h2>Create an Account</h2>
+      <p>Join LearnHub to start your learning journey.</p>
 
-       </div> 
-    )
+      <label for="name">Full Name</label>
+      <div class="input-group">
+        <span ><i class="fa-solid fa-person"></i></span>
+        <input type="text" id="name" placeholder="John Doe"/>
+      </div>
+
+      <label for="email">Email Address</label>
+      <div class="input-group">
+        <span ><i class="fa-solid fa-Email"></i></span>
+        <input type="email" id="email" placeholder="you@example.com"/>
+      </div>
+
+      <label for="password">Password</label>
+      <div class="input-group">
+        <span><i class="fa-solid fa-lock"></i></span>
+        <input type="password" id="password" placeholder="********"/>
+        <span><i class="fa-solid fa-eye"></i></span>
+      </div>
+
+      <label for="confirm-password">Confirm Password</label>
+      <div class="input-group">
+        <span><i class="fa-solid fa-lock"></i></span>
+        <input type="password" id="confirm-password" placeholder="********"/>
+        <span><i class="fa-solid fa-eye"></i></span>
+      </div>
+
+      <button type="submit" class="signup-button">Sign Up</button>
+
+      <div class="links">
+        <p>Already have an account? <a href="#">Log In</a></p>
+      </div>
+    </form>
+  </div>
+    );
 }
